0c6d6615cddd9693b309c0ba2f64ad2a0553b6d7
Marcella Mancinetti <marcella.mancinetti@gooddata.com>
2020-12-04 20:01:50 +0100

    Ready for prod

--- cpu_usage.py
+++ cpu_usage.py
@@ -18,20 +18,14 @@ def print_top_1():
 
     top = subprocess.Popen('top -b -n 1 -w 512|grep marci', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
     top_comm = top.communicate()
-    splitted_top = re.split('\n',top_comm[0])
     #print(top_comm)
-    #print(splitted_top[0])
-    #print(splitted_top[5])
+    splitted_top = re.split('\n',top_comm[0])
+    del splitted_top[-1]
     #print(splitted_top)
 
-    splitted_array = re.split(',',splitted_top[0])
     for i in splitted_top:
-      #print(splitted_array)
-      split = re.split('\s+',splitted_array[0])
+      split = re.split('\s+',i)
       print(split[9])
-
-
-
 
 
 # Press the green button in the gutter to run the script.
