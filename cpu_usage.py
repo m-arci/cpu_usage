fd6dd3b5dab2daad1ce9b5211e5a9b96fde28efb
Marcella Mancinetti <marcella.mancinetti@gooddata.com>
2020-12-04 15:20:50 +0100

    Primo test push

---
+++ cpu_usage.py
@@ -0,0 +1,38 @@
+import subprocess
+import re
+
+def print_top():
+
+    top = subprocess.Popen('top -b -n 1 -w 512 | grep vhost', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+
+    top_comm = top.communicate()
+    print(top_comm)
+
+    # ensure correct float number parsing
+    # qemu_list = re.sub(',', '.', top.communicate()[0])
+    #qemu_list = re.sub(',', '.', top_comm[0])
+    #print(qemu_list)
+
+
+def print_top_1():
+
+    top = subprocess.Popen('top -b -n 1 -w 512|grep marci', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    top_comm = top.communicate()
+    splitted_top = re.split('\n',top_comm[0])
+    #print(top_comm)
+    #print(splitted_top[0])
+    #print(splitted_top[5])
+    print(splitted_top)
+
+#    splitted_array = splitted_top.split()
+#   print(splitted_array[8])
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_top_1()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
