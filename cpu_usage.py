b853be5c1b0fb62978d111d193d8b4e2f96ca9ed
Marcella Mancinetti <marcella.mancinetti@gooddata.com>
2020-12-04 20:02:35 +0100

    Ready for prod

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,19 +1,6 @@
 import subprocess
 import re
 
-def print_top():
-
-    top = subprocess.Popen('top -b -n 1 -w 512 | grep marci', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-
-    top_comm = top.communicate()
-    print(top_comm)
-
-    # ensure correct float number parsing
-    #qemu_list = re.sub(',', '.', top.communicate()[0])
-    qemu_list = re.sub(',', '.', top_comm[0])
-    print(qemu_list)
-
-
 def print_top_1():
 
     top = subprocess.Popen('top -b -n 1 -w 512|grep marci', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
@@ -32,4 +19,4 @@ def print_top_1():
 if __name__ == '__main__':
     print_top_1()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
